2911b9dd7f1c24226c95071fc5e9f2892efda144
mr-cerebro <mrcerebro8@gmail.com>
2022-08-14 18:39:39 +0100

    update file main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const monthsPT = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+    const tableDays = document.getElementById('dias')
     function GetDaysCalendar(mes, ano) {
         document.getElementById('mes').innerHTML = monthsPT[mes]
+        document.getElementById('ano').innerHTML = ano
+
+        let firstDayOfWeek = new Date(mes, ano, 1).getDay() - 1 
+        let getLastDayThisMonth = new Date(mes, ano + 1, 0).getDay()
     }
     GetDaysCalendar(7, 2000)
 })
