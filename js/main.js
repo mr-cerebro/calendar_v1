4931b16252901f173ac66961170bc249806e94f4
mr-cerebro <mrcerebro8@gmail.com>
2022-08-14 18:24:32 +0100

    update file main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const monthsPT = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+    function GetDaysCalendar(mes, ano) {
+        document.getElementById('mes').innerHTML = monthsPT[mes]
+    }
+    GetDaysCalendar(1, 2000)
+})
